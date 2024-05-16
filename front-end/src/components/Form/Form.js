3f94ffe6da43e84af1b9fd4f9bc7e566fe1d29b0
Caio André <caioandresgit@outlook.com>
2024-05-16 18:49:03 -0300

    feat(Front-end): Styles componentized, requeste crud made with Axios.

---
+++ front-end/src/components/Form/Form.js
@@ -0,0 +1,88 @@
+import axios from "axios";
+import React, { useEffect, useRef } from "react";
+import { toast } from "react-toastify";
+
+const Form = ({ getUsers, onEdit, setOnEdit }) => {
+  const ref = useRef();
+
+  useEffect(() => {
+    if (onEdit) {
+      const user = ref.current;
+
+      user.name.value = onEdit.name;
+      user.email.value = onEdit.email;
+      user.phone.value = onEdit.phone;
+      user.birth_date.value = onEdit.birth_date;
+    }
+  }, [onEdit]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const user = ref.current;
+
+    if (
+      !user.name.value ||
+      !user.email.value ||
+      !user.phone.value ||
+      !user.birth_date.value
+    ) {
+      return toast.warn("Preencha todos os campos!");
+    }
+
+    if (onEdit) {
+      await axios
+        .put("http://localhost:8800/" + onEdit.id, {
+          name: user.name.value,
+          email: user.email.value,
+          phone: user.phone.value,
+          birth_date: user.birth_date.value,
+        })
+        .then(({ data }) => toast.success(data))
+        .catch(({ data }) => toast.error(data));
+    } else {
+      await axios
+        .post("http://localhost:8800", {
+          name: user.name.value,
+          email: user.email.value,
+          phone: user.phone.value,
+          birth_date: user.birth_date.value,
+        })
+        .then(({ data }) => toast.success(data))
+        .catch(({ data }) => toast.error(data));
+    }
+
+    user.name.value = "";
+    user.email.value = "";
+    user.phone.value = "";
+    user.birth_date.value = "";
+
+    setOnEdit(null);
+    getUsers();
+  };
+
+  return (
+    <FormContainer ref={ref} onSubmit={handleSubmit}>
+      <InputArea>
+        <Label>name</Label>
+        <Input name="name" />
+      </InputArea>
+      <InputArea>
+        <Label>E-mail</Label>
+        <Input name="email" type="email" />
+      </InputArea>
+      <InputArea>
+        <Label>Telephone</Label>
+        <Input name="phone" />
+      </InputArea>
+      <InputArea>
+        <Label>Data de Nascimento</Label>
+        <Input name="birth_date" type="date" />
+      </InputArea>
+
+      <Button type="submit">SALVAR</Button>
+    </FormContainer>
+  );
+};
+
+export default Form;
