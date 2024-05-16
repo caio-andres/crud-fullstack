c0b7ad4bfbe3320138a1c2fa6fdf5bb43dc70f79
Caio André <caioandresgit@outlook.com>
2024-05-16 18:37:23 -0300

    feat(Front-end): React app initialized and global style created.

---
+++ front-end2/src/App.js
@@ -0,0 +1,18 @@
+import GlobalStyle from "./styles/global";
+import styled from "styled-components";
+import Form from "./components/Form.js";
+import Grid from "./components/Grid";
+import { useEffect, useState } from "react";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import axios from "axios";
+
+function App() {
+  return (
+    <>
+      <GlobalStyle />
+    </>
+  );
+}
+
+export default App;
