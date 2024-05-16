754f0d1266d36357e00afeb358c30c787e0ec775
Caio André <caioandresgit@outlook.com>
2024-05-16 17:55:44 -0300

    feat(Back-end): Routes and controllers created.

--- back-end/index.js
+++ back-end/index.js
@@ -1,11 +1,12 @@
 import express from "express";
 import cors from "cors";
+import userRoutes from "./routes/users.js"
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-app.listen(8800);
+app.use("/", userRoutes)
 
-console.log(process.env.DATABASE)
+app.listen(8800);
